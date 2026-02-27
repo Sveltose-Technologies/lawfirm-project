9053ea825b44bbde47c71ea6a285e7c5ddc15546
sarath294 <130480495+sarath294@users.noreply.github.com>
2026-02-27 09:14:46 -0500

    Delete src directory

--- src/services/apiMessages.js
+++
@@ -1,7 +0,0 @@
-export const API_MESSAGES = {
-  FETCH_SUCCESS: "Data fetched successfully!",
-  CREATE_SUCCESS: "Record created successfully!",
-  UPDATE_SUCCESS: "Record updated successfully!",
-  DELETE_SUCCESS: "Record deleted successfully!",
-  ERROR_DEFAULT: "Something went wrong, please try again.",
-};
